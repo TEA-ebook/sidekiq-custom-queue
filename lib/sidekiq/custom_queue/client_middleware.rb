550695643ad9c026a1b7ef72050ac7dd4fca5a98
Kévin Gomez <kgomez@tea-ebook.com>
2016-06-15 18:10:29 +0200

    Fix worker construction

--- lib/sidekiq/custom_queue/client_middleware.rb
+++ lib/sidekiq/custom_queue/client_middleware.rb
@@ -2,7 +2,7 @@ module Sidekiq
   module CustomQueue
     class ClientMiddleware
       def call(worker_class, msg, queue, _redis_pool)
-        worker = worker_class.constantize
+        worker = worker_class.is_a?(String) ? worker_class.constantize : worker_class
         msg['queue'] = worker.custom_queue(msg).to_s if worker.respond_to?(:custom_queue)
         yield
       end
